ba908fc83937051befce185adf79ab678bca3331
danish-sartaj9000 <154250739+danish-sartaj9000@users.noreply.github.com>
2024-01-24 11:19:35 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,112 @@
+// console.log("Hello Pakistan") // console
+
+// // document.write("Show this on page") //Result in the page
+
+// // Variable
+
+// var a = "SMIT GenAI"
+// var a = "Saylani"
+// console.log(a)
+
+// const cnst = "Quaid e Azam"
+
+// // Data Types
+// var a = "SMIT GenAI"
+// var a = "Saylani"
+// console.log(a)
+
+// const d1 = "Quaid e Azam" //String 
+// const d2 = 12  //Number 
+// const d3 = 12.5 //Decimal 
+// const d4 = true //Boolean 
+// const d5 = false //Boolean 
+
+// // Array
+
+// var arr = ["Hammad", 12, true]
+// console.table(arr)
+
+
+// // alert("Hello to the Page")
+// // confirm("Do you want to proceed?")
+
+// var inp = prompt("Enter the Number")
+// console.log(inp)
+// document.write(`${inp} x 1 = `, inp*1 + "<br>")
+// document.write(`${inp} x 2 = `, inp*2 + "<br>")
+// document.write(`${inp} x 3 = `, inp*3 + "<br>")
+// document.write(`${inp} x 4 = `, inp*4 + "<br>")
+// document.write(`${inp} x 5 = `, inp*5 + "<br>")
+// document.write(`${inp} x 6 = `, inp*6 + "<br>")
+// document.write(`${inp} x 7 = `, inp*7 + "<br>")
+// document.write(`${inp} x 8 = `, inp*8 + "<br>")
+// document.write(`${inp} x 9 = `, inp*9 + "<br>")
+// document.write(`${inp} x 10 = `, inp*10 + "<br>")
+// document.write(`${inp} x 11 = `, inp*11 + "<br>")
+// document.write(`${inp} x 12 = `, inp*12 + "<br>")
+// document.write(`${inp} x 13 = `, inp*13 + "<br>")
+// document.write(`${inp} x 14 = `, inp*14 + "<br>")
+// document.write(`${inp} x 15 = `, inp*15 + "<br>")
+// document.write(`${inp} x 16 = `, inp*16 + "<br>")
+// document.write(`${inp} x 17 = `, inp*17 + "<br>")
+// document.write(`${inp} x 18 = `, inp*18 + "<br>")
+// document.write(`${inp} x 19 = `, inp*19 + "<br>")
+// document.write(`${inp} x 20 = `, inp*20 + "<br>")
+
+
+
+// console.log
+
+// console.log(`${inp} x 1 = `, inp*1 + "<br>")
+// console.log(`${inp} x 2 = `, inp*2 + "<br>")
+// console.log(`${inp} x 3 = `, inp*3 + "<br>")
+// console.log(`${inp} x 4 = `, inp*4 + "<br>")
+// console.log(`${inp} x 5 = `, inp*5 + "<br>")
+// console.log(`${inp} x 6 = `, inp*6 + "<br>")
+// console.log(`${inp} x 7 = `, inp*7 + "<br>")
+// console.log(`${inp} x 8 = `, inp*8 + "<br>")
+// console.log(`${inp} x 9 = `, inp*9 + "<br>")
+// console.log(`${inp} x 10 = `, inp*10 + "<br>")
+
+// // Arthematic Operators
+
+// var data1 = 90
+// var data2 = 10
+// var result1 = data1 + data2
+// var result2 = data1 - data2
+// var result3 = data1 * data2
+// var result4 = data1 / data2
+// var result5 = data1 % data2
+
+
+// console.log(result1)
+// console.log(result2)
+// console.log(result3)
+// console.log(result4)
+// console.log(result5)
+
+
+// // Assingment Operators
+
+
+
+// let x = 3;
+// const y = ++x; // 4
+// const y1 = --x; //3
+// const y = ++x;
+// console.log(y)
+// console.log(y1)
+const ptr = prompt("Tell me Your Percentage")
+
+if (ptr<= 50) {
+   document.write("Your are Pass. Phr bhi prh le bhai")
+} else if(ptr<= 60){
+   document.write("Grade B")
+}else if(ptr<= 70){
+   document.write("Grade A")
+}else if(ptr<= 80){
+   document.write("Grade A-1")
+}else if(ptr<= 100){
+   document.write("Grade A-1+")
+}else{
+   document.write("Please Input the correct data")}
